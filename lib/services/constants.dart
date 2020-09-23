0f42b7edf9bf15325740ca0ff8b71d06d7aa1b51
Asra/2000 <foreverasra@gmail.com>
2020-09-23 21:03:36 +0530

    add constant file

---
+++ lib/services/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color lightPinkColor = Color(0xFFFCCBCB);
+Color darkPink = Color(0xFFD9ADAD);
+Color blackPink = Color(0xFFAD9D9D);
+Color grey = Color(0xFF838383);
+Color brickRed = Color(0xFFD9968F);
+Color pale = Color(0xFFE9C6AE);
+Color deepBlue = Color(0xFF0C84C6);
