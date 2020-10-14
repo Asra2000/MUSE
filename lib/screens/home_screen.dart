8af381e7a8ffd9f884bc08a71e08ac400f9ff1e8
Asra/2000 <foreverasra@gmail.com>
2020-10-14 16:39:23 +0530

    modify UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,26 +6,33 @@ import '../services/networking.dart';
 import '../services/bottomNavBar.dart';
 
 class HomeScreen extends StatelessWidget {
-  final NetworkHelper net = new NetworkHelper(url :'http://api.shoutcast.com/station/nowplaying?ct=rihanna&f=json&k=qKAe6Vw5lR8EZNbn');
+  final NetworkHelper net = new NetworkHelper(
+      url:
+          'http://api.shoutcast.com/station/nowplaying?ct=rihanna&f=json&k=qKAe6Vw5lR8EZNbn');
+
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Stack(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.4,
+        child: Stack(
+          children: [
+            Positioned(
+              top: -(height * .25),
+              left: -(height / 2 - width / 2),
+              child: Container(
+                width: height,
+                height: height,
                 decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.only(
-                      bottomRight: Radius.circular(200.0),
-                      bottomLeft: Radius.circular(5.0)),
+                  color: lightPurple.withAlpha(100),
+                  shape: BoxShape.circle,
                 ),
               ),
-              Padding(
+            ),
+            SingleChildScrollView(
+              child: Padding(
                 padding: const EdgeInsets.symmetric(
                     vertical: 40.0, horizontal: 15.0),
                 child: Column(
@@ -33,14 +40,17 @@ class HomeScreen extends StatelessWidget {
                   children: [
                     Text(
                       "WHAT EVER YOUR MOOD WE ARE HERE TO LIGHT IT UP",
-                      style: TextStyle(color: Colors.white, fontSize: 20.0),
+                      style: TextStyle(color: grey, fontSize: 20.0),
                     ),
                     SizedBox(
                       height: 60.0,
                     ),
                     Text(
                       "HERE ARE SOME TOP HITS",
-                      style: TextStyle(color: lightPinkColor, fontSize: 20.0, fontWeight: FontWeight.bold),
+                      style: TextStyle(
+                          color: Colors.pink,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold),
                     ),
                     SizedBox(
                       height: 50.0,
@@ -48,55 +58,102 @@ class HomeScreen extends StatelessWidget {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        SongCards(id: "99476756", logo: 'http://i.radionomy.com/document/radios/c/c680/c6800ef5-f347-4f6e-9f55-22dd8ae43957.jpg',),
-                        SongCards(id: "99502558", logo: 'http://i.radionomy.com/document/radios/e/eaff/eaffb379-6436-4d27-876b-2a4dfcf54b26.png',),
+                        SongCards(
+                          id: "99476756",
+                          logo:
+                              'http://i.radionomy.com/document/radios/c/c680/c6800ef5-f347-4f6e-9f55-22dd8ae43957.jpg',
+                        ),
+                        SongCards(
+                          id: "99502558",
+                          logo:
+                              'http://i.radionomy.com/document/radios/e/eaff/eaffb379-6436-4d27-876b-2a4dfcf54b26.png',
+                        ),
                       ],
                     ),
-                    SizedBox(height: 15.0,),
+                    SizedBox(
+                      height: 25.0,
+                    ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        SongCards(id: "99367199", logo: 'http://i.radionomy.com/document/radios/4/4e70/4e701cc0-e2df-401c-9af0-18453804caed.jpg'),
-                        SongCards(id: "1623071",logo: 'https://www.listenonlineradio.com/wp-content/uploads/Oye-India-Radio.jpg',),
+                        SongCards(
+                            id: "99367199",
+                            logo:
+                                'http://i.radionomy.com/document/radios/4/4e70/4e701cc0-e2df-401c-9af0-18453804caed.jpg'),
+                        SongCards(
+                          id: "1623071",
+                          logo:
+                              'https://www.listenonlineradio.com/wp-content/uploads/Oye-India-Radio.jpg',
+                        ),
                       ],
                     ),
-                    SizedBox(height: 15.0,),
+                    SizedBox(
+                      height: 25.0,
+                    ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        SongCards(id: "99384331", logo: 'http://i.radionomy.com/document/radios/3/3717/37178dd7-0d0a-4c63-b04a-d4b4937a5486.png',),
-                        SongCards(id: "99498352", logo: 'http://i.radionomy.com/document/radios/b/bdd4/bdd4b069-8a86-4d1f-b918-4aadcce751a1.png',),
+                        SongCards(
+                          id: "99384331",
+                          logo:
+                              'http://i.radionomy.com/document/radios/3/3717/37178dd7-0d0a-4c63-b04a-d4b4937a5486.png',
+                        ),
+                        SongCards(
+                          id: "99498352",
+                          logo:
+                              'http://i.radionomy.com/document/radios/b/bdd4/bdd4b069-8a86-4d1f-b918-4aadcce751a1.png',
+                        ),
                       ],
                     ),
-                    SizedBox(height: 15.0,),
+                    SizedBox(
+                      height: 25.0,
+                    ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        SongCards(id: "99498352", logo: 'http://i.radionomy.com/document/radios/7/7b3f/7b3f524b-86e8-43aa-894c-6d73d1510580.jpg',),
-                        SongCards(id: "99304647",logo: 'http://i.radionomy.com/document/radios/7/7d44/7d44daa4-709e-4223-ba77-b04f1adc4a70.png',),
+                        SongCards(
+                          id: "99498352",
+                          logo:
+                              'http://i.radionomy.com/document/radios/7/7b3f/7b3f524b-86e8-43aa-894c-6d73d1510580.jpg',
+                        ),
+                        SongCards(
+                          id: "99304647",
+                          logo:
+                              'http://i.radionomy.com/document/radios/7/7d44/7d44daa4-709e-4223-ba77-b04f1adc4a70.png',
+                        ),
                       ],
                     ),
-                    SizedBox(height: 15.0,),
+                    SizedBox(
+                      height: 25.0,
+                    ),
                     //bollywood
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        SongCards(id: "1798887", logo: 'http://i.radionomy.com/document/radios/9/9070/907013f3-3406-43c1-842c-ba8750ccfcaf.jpg',),
-                        SongCards(id: "99498900",logo: 'http://i.radionomy.com/document/radios/f/fed6/fed6376f-2553-4f42-8bdb-63e827bca452.jpg',),
+                        SongCards(
+                          id: "1798887",
+                          logo:
+                              'http://i.radionomy.com/document/radios/9/9070/907013f3-3406-43c1-842c-ba8750ccfcaf.jpg',
+                        ),
+                        SongCards(
+                          id: "99498900",
+                          logo:
+                              'http://i.radionomy.com/document/radios/f/fed6/fed6376f-2553-4f42-8bdb-63e827bca452.jpg',
+                        ),
                       ],
                     )
                   ],
                 ),
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
       ),
       bottomNavigationBar: BottomAppBar(
         child: CustomBottomNavBar(),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           net.getTopRadio();
         },
       ),
@@ -107,6 +164,7 @@ class HomeScreen extends StatelessWidget {
 class SongCards extends StatelessWidget {
   final String id;
   final String logo;
+
   SongCards({this.id, this.logo});
 
   @override
@@ -115,10 +173,22 @@ class SongCards extends StatelessWidget {
       child: Container(
         width: 150.0,
         height: 150.0,
-        child: logo !=null ? Image.network(logo, width: 150.0, height: 150.0,) : null,
         decoration: BoxDecoration(
-            color: lightPinkColor.withAlpha(100),
-      ),
+          color: lightPinkColor.withAlpha(100),
+          shape: BoxShape.circle,
+            image: logo != null ? DecorationImage(
+              image: NetworkImage(logo),
+                fit: BoxFit.cover
+            ) : null,
+            boxShadow: [
+              BoxShadow(
+                offset: const Offset(3.0, 3.0),
+                blurRadius: 5.0,
+                spreadRadius: 6.0,
+                color: lightPurple
+              ),
+            ]
+        ),
       ),
       onTap: () async {
         NetworkHelper net = NetworkHelper();
--- lib/services/constants.dart
+++ lib/services/constants.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 Color lightPinkColor = Color(0xFFFCCBCB);
 Color darkPink = Color(0xFFD9ADAD);
 Color blackPink = Color(0xFFAD9D9D);
+Color lightPurple = Color(0xFFCBADD3);
 Color grey = Color(0xFF838383);
 Color brickRed = Color(0xFFD9968F);
 Color pale = Color(0xFFE9C6AE);
